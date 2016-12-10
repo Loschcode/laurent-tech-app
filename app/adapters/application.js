5a4f0bf49ab4d2dbbb6038b96b586edd0616eb46
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-10 19:53:30 +0100

    more console log

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
+console.log("api or something");
+
 export default DS.RESTAdapter.extend({
   namespace: window.FeedApp.api.namespace,
   host: window.FeedApp.api.host
