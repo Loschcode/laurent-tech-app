25a2df3d0e9c48eb68b44367e13a64e5b471be20
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-10 19:31:23 +0100

    changes configuring the api endpoint + dynamics

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api'
+  namespace: window.FeedApp.api.namespace,
+  host: window.FeedApp.api.host
 });
--- config/environment.js
+++ config/environment.js
@@ -14,6 +14,10 @@ module.exports = function(environment) {
     },
 
     APP: {
+      api: {
+        host: 'http://feed-api.laurent.tech',
+        namespace: '/'
+      },
       myself: {
         github: 'https://github.com/Loschcode',
         linkedin: 'http://fr.linkedin.com/in/laurentschaffner',
@@ -44,6 +48,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+
+    ENV.APP.api.host = 'http://feed-api.laurent.tech';
+    ENV.APP.api.namespace = '/';
 
   }
 
