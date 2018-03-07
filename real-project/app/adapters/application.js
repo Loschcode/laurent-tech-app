b3cd663354ea04103e8d95cb8c1018fbc3b69335
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-03-07 23:01:16 +0100

    subfolder removed just in case

--- real-project/app/adapters/application.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-import ENV from 'feed-app/config/environment';
-
-export default DS.RESTAdapter.extend({
-  namespace: ENV.api.namespace,
-  host: ENV.api.host
-});
--- real-project/app/helpers/relative-date.js
+++
@@ -1,6 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function([date]) {
-  /* eslint-disable no-undef */
-  return moment(date).fromNow();
-});
