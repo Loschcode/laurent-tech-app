511c6dadf62e0fd8bf4b04e19f90bc52c29b9e86
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-03-13 01:17:01 +0100

    fastboot added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -38,7 +38,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   //
-  app.import('node_modules/gridle/js/gridle-full.min.js');
+  // app.import('node_modules/gridle/js/gridle-full.min.js');
   app.import('node_modules/moment/moment.js');
 
   return app.toTree();
