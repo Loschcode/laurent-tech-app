df3d1242ddd75ebe49e7af8aac186ff07eaf718a
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-06-11 16:39:41 -0400

    refresh projects and stuff

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -9,7 +9,7 @@
             <h1><span class="icon-introduction"></span> Introduction</h1>
             <!-- Description -->
             <p>
-              I've been working on side projects since I'm a teenager, so far I can proudly say that none of my tech-oriented projects was ever a success, but I don't give up and keep a trace of each of them, hoping someday someone will ask about it.
+              I've been working on side projects since I'm a teenager, so far I can proudly say that none of my tech-oriented projects was ever a success, but I don't give up and keep a trace of each of them, hoping someday someone will ask about it so I can keep on giving at parties. I've solely listed the big projects I realized, no libraries or viable businesses are quoted.
             </p>
           </div>
 
@@ -18,20 +18,23 @@
           <div class="resume__experience">
             <!-- Title -->
             <h1><span class="icon-current-projects"></span> Current projects</h1>
-            <!-- Job -->
-            <h2>Bloodbath.io</h2>
-            <h3>VueJS SPA / Rails API</h3>
+            <!-- AskAlfred -->
+            <h2>AskAlfred</h2>
+            <h3>VueJS / GraphQL / Rails API</h3>
             <div class="resume__detail">
-              Since January 2018
+              Since January 2019
             </div>
             <!-- Description -->
             <p>
-              Ever heard of cryptocurrencies ? Well, me too. After investing my money in this emotional rollercoaster since early 2017, I realized no portfolio was ever matching my expectations, so I built one. Also crypto projects were being created all over the Internet, so why not make one too ? I tried to keep everything simple and minimalist. The killer feature is the weather which illustrates the current trend of the daily market or/and your portfolio via a secret algorithm anyone could guess.
+              It's been years cool services pop up all over the place. You have Uber, AirBnB and what not. Surely everyone wants to be in the next craze, but people all look at the same direction, like food delivery or car sharing. There's one domain which hasn't been touched by a robust app though; conciergerie. After trying a few of them for my own needs, I realized how bad all that was orchestrated. That's how Alfred was born. I decided to write up a cool and very accessible app you can use H24 7/7 from any device, to rapidly ask for little tasks without second thought. Ask him something, and he will do it for you. You avoid burning your precious time, and the stress of sending a letter, translating something or call the plumber vanishes. Delegating tasks has never been this easy.
+            </p>
+            <p>
+              Alfred is by far the most complete app I've ever built. I took months designing the graphics through Sketch, writing up a realistic business model and plan, defining the MVP specs and pushing the marketing ideas upside down with Notion. It's currently under beta-testing, I've given myself one year to make it work commercially.
             </p>
             <!-- Bottom -->
             <div class="resume__bottom">
               <div class="external-link">
-                <a href="https://bloodbath.io" target="_blank"><span class="link">View project online</span> <span class="icon icon-link"></span></a>
+                <a href="https://askalfred.to" target="_blank"><span class="link">View project online</span> <span class="icon icon-link"></span></a>
               </div>
             </div>
           </div>
@@ -41,12 +44,58 @@
 
             <!-- Title -->
             <h1><span class="icon-projects-graveyard"></span> Projects graveyard</h1>
+
+            <!-- Bloodbath -->
+            <h2>Bloodbath.io</h2>
+            <h3>VueJS SPA / Rails API</h3>
+            <div class="resume__detail">
+              January 2018 - September 2018
+            </div>
             <!-- Description -->
             <p>
-              Coming soon
+              Ever heard of cryptocurrencies ? Well, me too. After investing my money in this emotional rollercoaster since early 2017, I realized no portfolio was ever matching my expectations, so I built one. Also crypto projects were being created all over the Internet, so why not make one too ? I tried to keep everything simple and minimalist. The killer feature is the weather which illustrates the current trend of the daily market or/and your portfolio via a secret algorithm anyone could guess, because it's on Github.
             </p>
-          </div>
+            <!-- Bottom -->
+            <div class="resume__bottom">
+              <div class="external-link">
+                <a href="https://github.com/Loschcode/bloodbath-app" target="_blank"><span class="link">View on Github</span> <span class="icon icon-link"></span></a>
+              </div>
+            </div>
 
+            <!-- Geokaliz -->
+            <h2>Geokaliz</h2>
+            <h3>Full MeteorJS</h3>
+            <div class="resume__detail">
+              August 2015 - December 2015
+            </div>
+            <!-- Description -->
+            <p>I've always wanted to build up a native app. While learning Swift and making a very bad Beeper MVP, I discovered MeteorJS, a very promising technology, you could build up an Android / iOS / MacOS app all at once through Cordova and much more. I decided to code this small product about geolocation. If you're ever looking for your friends, you can open up Geokaliz, show your location with a green icon and your friend will be able to catch up with you via this open stream. The whole app was composed as live location system and published to the App Store, Google Play and I also made a MacOS version based in Electron.</p>
+            <!-- Bottom -->
+            <div class="resume__bottom">
+              <div class="external-link">
+                <a href="https://github.com/Loschcode/legacy-geokaliz" target="_blank"><span class="link">View on Github</span> <span class="icon icon-link"></span></a>
+              </div>
+            </div>
+
+            <!-- Unicorn FM -->
+            <h2>Unicorn FM</h2>
+            <h3>Javascript / NodeJS</h3>
+            <div class="resume__detail">
+              May 2014 - May 2015
+            </div>
+            <!-- Description -->
+            <p>Ever been to a cool party ? Well, me neither, but for the ones who went, an important criteria to a good party is the sound system, and it often ends up with a computer plugged to a high-fi, sometimes a phone / tablet. The thing is, who wants to move around to change the music ? No one. And who wants to buy those expensive connected sound systems ? No one either.</p>
+            <p>I decided to create a synchronization app available from any computer or mobile device. In short, you open your browser / app and go to your channel as a speaker, then anyone can go to the same channel and switch musics, change the volume or add the next music to be played. The area you can connect to wasn’t limited like Bluetooth based systems.</p>
+            <p>The project had its market ; people were using it at different parties around my city but it was based on YouTube API which changed and became very restricted in term of auto play. I had to give it up, but plan on making a come back someday.</p>
+            <!-- Bottom -->
+            <div class="resume__bottom">
+              <div class="external-link">
+                <a href="https://github.com/Loschcode/legacy-unicorn-fm" target="_blank"><span class="link">View on Github</span> <span class="icon icon-link"></span></a>
+              </div>
+            </div>
+
+            <p><strong>I've got many other past projects, but I didn't give myself time to write them up yet...</strong></p>
+          </div>
           <hr />
         </div>
       </div>
