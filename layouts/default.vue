fdf0ae7edbb5cd73dcc11b6a050f2ceca4db1818
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-07-14 14:53:16 -0400

    reversed icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,8 +28,8 @@
               <a href="https://github.com/Loschcode" target="_blank"><i aria-hidden="true" class="fab fa-github"></i></a>
               <a href="http://fr.linkedin.com/in/laurentschaffner" target="_blank"><i aria-hidden="true" class="fab fa-linkedin"></i></a>
               <a href="https://twitter.com/LoschCode" target="_blank"><i aria-hidden="true" class="fab fa-twitter"></i></a>
-              <a href="https://www.facebook.com/LoSchaffner" target="_blank"><i aria-hidden="true" class="fab fa-medium"></i></a>
-              <a href="https://medium.com/@LoschCode" target="_blank"><i aria-hidden="true" class="fab fa-facebook"></i></a>
+              <a href="https://medium.com/@LoschCode" target="_blank"><i aria-hidden="true" class="fab fa-medium"></i></a>
+              <a href="https://www.facebook.com/LoSchaffner" target="_blank"><i aria-hidden="true" class="fab fa-facebook"></i></a>
               <a href="callto:loschcode" target="_blank"><i aria-hidden="true" class="fab fa-skype"></i></a>
             </div>
           </div>
